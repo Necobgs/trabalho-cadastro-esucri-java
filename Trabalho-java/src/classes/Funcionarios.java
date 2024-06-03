6667bf49784672fa2fe664104cbce5cc569b1a54
Necobgs <emanuelborgesdev@gmail.com>
2024-06-03 18:13:36 -0300

    first commit

---
+++ Trabalho-java/src/classes/Funcionarios.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author User
+ */
+public class Funcionarios {
+    private String nome;
+    private String cpf;
+    private Double salario;
+    private Double inss;
+    
+    public Funcionarios(String nome,
+                        String cpf,
+                        Double salario){
+        
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+        
+        
+    }
+    
+}
